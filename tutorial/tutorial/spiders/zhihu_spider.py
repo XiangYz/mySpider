7e27c62e4b591652cd09edc15a979ef237dd69ec
Xiangyz <5555wyhhhgxx@163.com>
2017-07-09 23:27:52 +0800

    bug fixed

--- tutorial/tutorial/spiders/zhihu_spider.py
+++ tutorial/tutorial/spiders/zhihu_spider.py
@@ -1,29 +1,42 @@
 import scrapy
 import os
 import urllib
+import requests
 
 class ZhihuSpider(scrapy.Spider):
 	name = "Zhihu"
-	#start_urls = ['https://www.zhihu.com/question/27761934',]
-	start_urls = ['http://www.ibantang.com',]
+	start_urls = ['https://www.zhihu.com/question/27761934',]
+	#start_urls = ['http://www.ibantang.com',]
 
 	def parse(self, response):
-		"""
-		for item in response.xpath("//div[@class='List-item']"):
-			user_name = item.xpath("//a[@class='UserLink-link']/text()").extract_first()
-			img_src = item.xpath("//img/@src/text()").extract()
-			
+		list_item = response.xpath("//div[@class='List-item']")
+		for item in list_item:
+			user = item.xpath(".//a[@class='UserLink-link']/text()").extract_first()
+			img_list = item.xpath(".//img/@src").extract()
 			i = 1
-			for img_item in img_src:
-				img_item_src = "www.zhihu.com" + img_item
-				file_name = user_name + "_" + str(i)
-				file_path = os.path.join('.\\zhihu_spider', file_name)
-				urllib.urlretrieve(img_item_src, file_path)
-		"""
+			for img_item in img_list:
+				file_name = user + "_" + str(i) + ".jpg"
+				file_path = os.path.join('D:\proj\pyprj\spider_img', file_name)
+				#urllib.urlretrieve(img_item, file_path)  #socket error
+				"""
+				urlopen = urllib.URLopener()
+				fp = urlopen.open(img_item)
+				imgdata = fp.read()
+				fileobj = open(file_path, 'wb')
+				fileobj.write(imgdata)
+				fileobj.close()
+				i = i + 1
+				"""
+				with requests.Session() as s:
+					#s.get(img_item)
+					with open(file_path, "wb") as f:
+					   f.write(s.get(img_item).content)
 		
+		
+		"""
 		i = 1
 		for item in response.xpath("//div[@class='topic-item-v3']"):
-			img_src = item.xpath("//img/@data-original/text()").extract()
+			img_src = item.xpath(".//img/@data-original/text()").extract()
 			
 			for img_item in img_src:
 				img_item_src = "www.ibantang.com" + img_item
@@ -31,5 +44,5 @@ class ZhihuSpider(scrapy.Spider):
 				i = i + 1
 				file_path = os.path.join('.\\ibantang_imgs', file_name)
 				urllib.urlretrieve(img_item_src, file_path)
-
+		"""
 
