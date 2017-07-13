7a1dc15950953e4142d9ab7b59f6b0b2cbb42acd
Xiangyz <5555wyhhhgxx@163.com>
2017-07-13 18:21:28 +0800

    bug fixed

--- tutorial/tutorial/spiders/jiandan_spider.py
+++ tutorial/tutorial/spiders/jiandan_spider.py
@@ -24,17 +24,30 @@ class ZhihuSpider(scrapy.Spider):
     def parse(self, response):
         list_item = response.xpath("//ol[@class='commentlist']/li")
         for item in list_item:
-            img_list = item.xpath(".//img/@src").extract()
-            for img_item in img_list:
-                file_name = "jiandan_" + str(self.i)
-                self.i = self.i + 1
-                file_path = os.path.join('D:\\xiang\\github_space\\jiandan_pic', file_name)
-                content = urllib2.urlopen("http:" + img_item).read()
-                imgtype = imghdr.what('', h = content)
-                if not imgtype:
-                    imgtype = 'txt'
-                with open(file_path + "." + imgtype, 'wb') as picfile:
-                    picfile.write(content)
+
+            img_item = item.xpath(".//img/@src").extract_first()
+            if not img_item:
+                continue
+            pos = img_item.rfind('.gif')
+            if pos > 0:
+                img_item = item.xpath(".//img/@org_src").extract_first()
+                if not img_item:
+                continue
+            file_name = "jiandan_" + str(self.i)
+            self.i = self.i + 1
+            file_path = os.path.join('D:\\xiang\\github_space\\jiandan_pic', file_name)
+            content = urllib2.urlopen("http:" + img_item).read()
+
+            if not content:
+                continue
+            imgtype = imghdr.what('', h = content)
+            if not imgtype:
+                continue
+            
+            with open(file_path + "." + imgtype, 'wb') as picfile:
+                picfile.write(content)
+
+            
 
         next_url = response.xpath("//div[@class='cp-pagenavi']/a[@class='previous-comment-page']/@href").extract_first()
         if next_url:
