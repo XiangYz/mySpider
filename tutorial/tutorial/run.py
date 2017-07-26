2836e9a9ea84a3d558d7a9833cf4605cf132cb31
Xiangyz <5555wyhhhgxx@163.com>
2017-07-26 18:34:28 +0800

    run.py updated

--- tutorial/tutorial/run.py
+++ tutorial/tutorial/run.py
@@ -2,7 +2,14 @@
 
 
 from scrapy import cmdline
+import os
 
+
+target_path = 'C:\\jiandan_pic'
+list_file = [x for x in os.listdir(target_path)]
+for f in list_file:
+    abs_f = os.path.join(target_path, f)
+    os.remove(abs_f)
 
 name = 'jiandan'
 cmd = 'scrapy crawl {0}'.format(name)
